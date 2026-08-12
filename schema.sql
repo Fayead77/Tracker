eec7c1fe70d82d94451c46cd1eec3e7a49550347
Fayead Mahim <muntasirmahim.35@gmail.com>
2026-08-13 02:18:24 +0600

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
+
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS categories;
 DROP TABLE IF EXISTS subjects;
@@ -31,6 +32,7 @@ CREATE TABLE subjects (
     level2_label TEXT NOT NULL DEFAULT 'Module',
     pinned INTEGER NOT NULL DEFAULT 0,
     sort_order INTEGER DEFAULT 0,
+    is_flat INTEGER NOT NULL DEFAULT 0,
     created_at TEXT DEFAULT (datetime('now')),
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE
