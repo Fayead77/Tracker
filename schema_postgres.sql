fcd012fbc2d8495fc37456f759b48893fc722758
Fayead Mahim <muntasirmahim.35@gmail.com>
2026-08-13 02:18:59 +0600

    Update schema_postgres.sql

--- schema_postgres.sql
+++ schema_postgres.sql
@@ -21,6 +21,7 @@ CREATE TABLE IF NOT EXISTS subjects (
     level2_label TEXT NOT NULL DEFAULT 'Module',
     pinned INTEGER NOT NULL DEFAULT 0,
     sort_order INTEGER DEFAULT 0,
+    is_flat INTEGER NOT NULL DEFAULT 0,
     created_at TEXT DEFAULT to_char(now(), 'YYYY-MM-DD HH24:MI:SS')
 );
 
